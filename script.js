1818a302a1f92cf28658859c1eab100f4d6b24f7
SRShaikh <srshaikh.work@gmail.com>
2021-10-29 08:09:45 +0530

    29 oct 21 morning

--- script.js
+++ script.js
@@ -1,6 +1,41 @@
 (function () {
 
-  var swiper = new Swiper(".testimonials-slider", {
+
+  new Swiper('.clients-slider', {
+    speed: 400,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    slidesPerView: 'auto',
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 2,
+        spaceBetween: 40
+      },
+      480: {
+        slidesPerView: 3,
+        spaceBetween: 60
+      },
+      640: {
+        slidesPerView: 4,
+        spaceBetween: 80
+      },
+      992: {
+        slidesPerView: 5,
+        spaceBetween: 120
+      }
+    }
+  })
+
+  // ------------------ Testimonials-slider ----------------------
+  new Swiper(".testimonials-slider", {
     // slidesPerView: 3,
     speed: 600,
     loop: true,
