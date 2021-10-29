7275c35f0351ec4e4d9c4bda8930ae165ae61f31
SRShaikh <srshaikh.work@gmail.com>
2021-10-29 07:33:45 +0530

    team added

--- script.js
+++ script.js
@@ -1,28 +1,29 @@
 (function () {
 
-  new Swiper('.testimonials-slider', {
+  var swiper = new Swiper(".testimonials-slider", {
+    // slidesPerView: 3,
     speed: 600,
     loop: true,
     autoplay: {
-      delay: 5000,
+      delay: 3000,
       disableOnInteraction: false
     },
     slidesPerView: 'auto',
     pagination: {
-      el: '.swiper-pagination',
+      el: ".swiper-pagination",
       type: 'bullets',
-      clickable: true
+      clickable: true,
     },
     breakpoints: {
       320: {
         slidesPerView: 1,
         spaceBetween: 40
       },
-
       1200: {
         slidesPerView: 3,
       }
     }
+
   });
 
 })();
