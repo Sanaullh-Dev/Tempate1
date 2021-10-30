cdcfe465212c73dab4a9c598b3c2bcbdbd62ae6c
Sanaulla <86148969+Sanaullh-Dev@users.noreply.github.com>
2021-10-30 16:32:36 +0530

    copyright and aos apply

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 (function () {
 
+  function aos_init() {
+    AOS.init({
+      duration: 1000,
+      easing: "ease-in-out",
+      once: true,
+      mirror: false
+    });
+  }
+  window.addEventListener('load', () => {
+    aos_init();
+  });
 
   new Swiper('.clients-slider', {
     speed: 400,
